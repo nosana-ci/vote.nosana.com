be46a5a7db7c7a159e9014ce0b05f56d71f0e692
Jairzijp <jairzijp123@gmail.com>
2025-11-06 15:14:19 +0100

    Initial commit

---
+++ app/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="container">
+      <h1 class="title is-1">Voting UI</h1>
+      <div class="columns">
+        <div class="column is-8">
+          <div class="box">
+            <p>Proposal</p>
+          </div>
+          <div class="box">
+            <p>Voting Options</p>
+          </div>
+        </div>
+        <div class="column is-4">
+          <div class="box">
+            <p>Progress</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,33 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  css: ["~/assets/styles/global.scss"],
+  modules: ['@nuxtjs/google-fonts', '@nuxtjs/color-mode'],
+  googleFonts: {
+    preload: true,
+    families: {
+      Outfit: [300, 400, 700],
+      "Space Grotesk": [400],
+    },
+  },
+  colorMode: {
+    preference: "light", // default value of $colorMode.preference
+    fallback: "light", // fallback value if not system preference found
+    hid: "nuxt-color-mode-script",
+    globalName: "__NUXT_COLOR_MODE__",
+    componentName: "ColorScheme",
+    classPrefix: "",
+    classSuffix: "-mode",
+    storageKey: "nuxt-color-mode",
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/styles/variables.scss" as *;',
+        },
+      },
+    },
+  },
+})
