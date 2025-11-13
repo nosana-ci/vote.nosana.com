7c9bc620519c6e458cf6e6a80559be0bb3548da3
Laurens Verspeek <l.verspeek@gmail.com>
2025-11-13 17:41:49 +0100

    Revert "temp disable voting interface"
    This reverts commit 28d4ca19e1f381fe9020ba276c0a912cb1cbdd93.

--- app/app.vue
+++ app/app.vue
@@ -2,9 +2,7 @@
   <div class="app-wrapper">
     <Topbar />
     <main class="main-content">
-      <div class="container">
-        <h1 class="title is-4 mt-6">Coming soon...</h1>
-      </div>
+      <NuxtPage />
     </main>
     <Footer />
   </div>
